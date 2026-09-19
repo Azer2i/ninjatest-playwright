51ee234085ee8edd0c838743874079a6e8bb13a3
Azer Ibrahimli <aibrahimov191@gmail.com>
2026-09-20 03:39:50 +0400

    add allure-cucumberjs reporter

--- cucumber.js
+++ cucumber.js
@@ -3,7 +3,14 @@ module.exports = {
     requireModule: ['tsx/cjs'],
     require: ['features/support/**/*.ts', 'features/step_definitions/**/*.ts'],
     paths: ['features/**/*.feature'],
-    format: ['progress-bar', 'html:reports/cucumber-report.html'],
+    format: [
+      'progress-bar',
+      'html:reports/cucumber-report.html',
+      'allure-cucumberjs/reporter',
+    ],
+    formatOptions: {
+      resultsDir: 'allure-results',
+    },
     publishQuiet: true,
   },
 };
